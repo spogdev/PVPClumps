83273ecac6805a42ff0c9624980481699141f832
spogdev <zekoursyt@gmail.com>
2026-02-09 22:04:16 +0000

    Initial commit

---
+++ src/main/java/live/spog/pvpClumps/ClumpsPlugin.java
@@ -0,0 +1,16 @@
+package live.spog.pvpClumps;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ClumpsPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        for ()
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
