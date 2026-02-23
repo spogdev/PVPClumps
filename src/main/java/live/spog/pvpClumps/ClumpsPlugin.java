2ff1527f9f69694245daec0703acd863f7b1ffd6
spogdev <zekoursyt@gmail.com>
2026-02-23 16:06:59 +0000

    init2

--- src/main/java/live/spog/pvpClumps/ClumpsPlugin.java
+++ src/main/java/live/spog/pvpClumps/ClumpsPlugin.java
@@ -1,16 +1,30 @@
 package live.spog.pvpClumps;
 
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.ExperienceOrb;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntitySpawnEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class ClumpsPlugin extends JavaPlugin {
+public final class ClumpsPlugin extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        for ()
+        getServer().getPluginManager().registerEvents(this, this);
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    @EventHandler(ignoreCancelled = true)
+    public void onXP(EntitySpawnEvent event) {
+        if (!(event.getEntity() instanceof ExperienceOrb orb)) return;
+        int total = orb.getExperience();
+        for (Entity e : orb.getNearbyEntities(3, 3, 3)) {
+            if (!(e instanceof ExperienceOrb other)) continue;
+            if (other == orb) continue;
+            total += other.getExperience();
+            other.remove();
+        }
+        orb.setExperience(total);
     }
+
 }
