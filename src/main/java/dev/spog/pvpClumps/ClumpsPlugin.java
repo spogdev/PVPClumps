1f942a250d1b20c060a786a3c70d5319f7fedf93
spogdev <zekoursyt@gmail.com>
2026-03-24 19:52:26 +0000

    - Corrected version numbering - Added bStats support

--- src/main/java/dev/spog/pvpClumps/ClumpsPlugin.java
+++ src/main/java/dev/spog/pvpClumps/ClumpsPlugin.java
@@ -1,5 +1,7 @@
-package live.spog.pvpClumps;
+package dev.spog.pvpClumps;
 
+import org.bstats.bukkit.Metrics;
+import org.bstats.charts.SimplePie;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.ExperienceOrb;
 import org.bukkit.event.EventHandler;
@@ -12,6 +14,7 @@ public final class ClumpsPlugin extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(this, this);
+        initBstats();
     }
 
     @EventHandler(ignoreCancelled = true)
@@ -27,4 +30,13 @@ public final class ClumpsPlugin extends JavaPlugin implements Listener {
         orb.setExperience(total);
     }
 
+    public void initBstats() {
+        int pluginId = 30378;
+        Metrics metrics = new Metrics(this, pluginId);
+
+        metrics.addCustomChart(
+                new SimplePie("chart_id", () -> "My value")
+        );
+        getLogger().info("Starting bStats instance using id: " + pluginId);
+    }
 }
